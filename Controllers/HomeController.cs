2540abac7dc765441ddf76783c434b8b2d0cafa5
AMÉLIE TORRE WALSH <47757058@ort>
2023-06-26 10:04:31 -0300

    Commit Base

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP06Elecciones_Gayoso_TorreWalsh.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP06Elecciones_Gayoso_TorreWalsh.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
