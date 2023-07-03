bc813c9c24391ab3c7ceda74b1a41774b74a8ea7
AMÉLIE TORRE WALSH <47757058@ort>
2023-07-03 10:23:59 -0300

    más cosas de base y funciones de BD

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,25 +6,32 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        ViewBag.detallePartido = BD.ListarPartidos();
         return View();
     }
     public IActionResult VerDetallePartido(int idPartido){
+        ViewBag.detallePartido = BD.VerInfoPartido(idPartido);
+        ViewBag.detalleCandidato = BD.ListarCandidatos(idPartido);
         return View("DetallePartido");
     }
     public IActionResult VerDetalleCandidato(int idCandidato){
+        ViewBag.detalleCandidato = BD.VerInfoCandidato(idCandidato);
         return View("DetalleCandidato");
     }
     public IActionResult AgregarCandidato(int idPartido){
+        ViewBag.ListaIdPartidos = idPartido;
         return View("CandidatoFormulario");
     }
-    [HttpPost] public IActionResult GuardarCandidato(Candidato Candidato.can){
-        return View("Detallepartido");
+    [HttpPost] public IActionResult GuardarCandidato(Candidato candidato){
+        BD.AgregarCandidato(candidato);
+        return RedirectToAction("DetallePartido", new { idPartido = candidato.IdPartido});
     }
     public IActionResult EliminarCandidato(int idCandidato, int idPartido){
         /*eliminar candidato
         Escape en partido candidatos - candidato seleccionado
         */
-        return View("DetallePartido");
+        BD.EliminarCandidato(idCandidato);
+        return RedirectToAction("DetallePartido", new { idPartido = idPartido});
     }
     public IActionResult Elecciones(){
         return View("EleccionesInfo");
--- Models/BD.cs
+++ Models/BD.cs
@@ -1,20 +1,59 @@
-static class BD{
-    public static void AgregarCandidato(Candidato Can){
+using System.Data.SqlClient;
+using Dapper;
+namespace TP06Elecciones_Gayoso_TorreWalsh;
+public class BD{
+     private static string ConnectionString = @"Server=localhost;DataBase=Elecciones;Trusted_Connection=True;";
+    public static void AgregarCandidato(Candidato candidato){
+        using (SqlConnection db = new SqlConnection(ConnectionString)){
+            string sql = "INSER INTO Candidato(ID_Candidato,ID_Partido,Apellido,Nombre,Nacimiento,Foto,Postulacion) VALUES (@pID_Candidato,@pID_Partido,@pApellido,@pNombre,@pNacimiento,@pFoto,@pPostulacion)";
+            db.Execute(sql, new { pID_Candidato = candidato.IdCandidato, pID_Partido = candidato.IdPartido, pApellido = candidato.Apellido, pNombre = candidato.Nombre, pNacimiento = candidato.FechaNacimiento, pFoto = candidato.Foto, pPostulacion = candidato.Postulacion});
+        }
         //agregar candidato a BD
     }
-    public static void EliminarCandidato(int idCandidato){
-        //elimina candidato de BD
+    public static void EliminarCandidato(int idCandidato)
+    {     
+        using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "DELETE FROM Candidato WHERE ID_Candidato = @pidCandidato";
+            db.Execute(sql, new { pidCandidato = idCandidato });
+        }
     }
-    public static void VerInfoPartido(int idPartido){
-        //adevuelve un objeto Partido
+    public static Partido VerInfoPartido(int idPartido){
+        
+        Partido partido = null;
+        using (SqlConnection db = new SqlConnection(ConnectionString)){
+            string sql = "SELECT * FROM Partido WHERE ID_Partido = @ID_Partido";
+            partido = db.QueryFirstOrDefault<Partido>(sql, new { pID_Partido = idPartido});
+        }
+        return partido;
     }
-    public static void VerInfoCandidato(int idCandidato){
-        //devuelve objeto Candidato
+    public static Candidato VerInfoCandidato(int idCandidato){
+        Candidato candidato = null;
+        using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "SELECT * FROM Candidato WHERE ID_Candidato = @pID_Candidato";
+            candidato = db.QueryFirstOrDefault<Candidato>(sql, new {pID_Candidato = idCandidato});
+        }
+        return candidato;
     }
-    public static void ListarPartidos(){
-        //devuelve list partidos
+    public static List<Partido> ListarPartidos(){
+        List <Partido> ListPartidos = new List<Partido>();
+        using (SqlConnection db = new SqlConnection(ConnectionString)){
+            string sql = "SELECT * FROM Partido";
+            ListPartidos = db.Query<Partido>(sql).ToList();
+        }
+        return ListPartidos;
     }
-    public static void ListarCandidatos(int idPartido){
-        //devuelve list candidatos
+    //List<Candidato> llama a la clase
+    public static List<Candidato> ListarCandidatos(int IdPartido)
+    {
+       List <Candidato> ListCandidatos = new List<Candidato>();
+        using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "SELECT * FROM Candidato WHERE ID_Partido = @IdPartido";
+            ListCandidatos = db.Query<Candidato>(sql, new{IDPartido=IdPartido}).ToList();
+        }
+        return ListCandidatos;
     }
+    //db,db.Query --> conexion base de datos
 }
